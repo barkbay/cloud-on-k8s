f83b4151d5b31d0d0738c4b5e993bf56487d1d92
Michael Morello <michael.morello@elastic.co>
2021-05-26 09:20:07 +0200

    Remove pod blocker webhook

--- pkg/controller/vacate/webhook/blocker.go
+++ pkg/controller/vacate/webhook/blocker.go
@@ -15,31 +15,18 @@ import (
 )
 
 const (
-	DelayAnnotation   = "elasticsearch.k8s.elastic.co/delay-creation"
-	TrustMeAnnotation = "elasticsearch.k8s.elastic.co/trust-me-i-m-an-engineer"
+	DelayAnnotation = "elasticsearch.k8s.elastic.co/delay-creation"
 )
 
 var (
 	pvcLog = ulog.Log.WithName("pvc-blocker")
-	podLog = ulog.Log.WithName("pod-blocker")
 )
 
 // +kubebuilder:webhook:path="/elastic-pvc-blocker-k8s-elastic-co-v1-elasticsearch",mutating=false,failurePolicy=Fail,groups="",resources=persistentvolumeclaims,verbs=create,versions=v1,name=elastic-pvc-blocker.k8s.elastic.co,sideEffects=None,admissionReviewVersions=v1;v1beta1,matchPolicy=Exact
 
-// +kubebuilder:webhook:path="/elastic-pod-blocker-k8s-elastic-co-v1-elasticsearch",mutating=false,failurePolicy=Fail,groups="",resources=pods,verbs=create,versions=v1,name=elastic-pod-blocker.k8s.elastic.co,sideEffects=None,admissionReviewVersions=v1;v1beta1,matchPolicy=Exact
-
 const (
 	pvcBlockerWebhookPath = "/elastic-pvc-blocker-k8s-elastic-co-v1-elasticsearch"
-	podBlockerWebhookPath = "/elastic-pod-blocker-k8s-elastic-co-v1-elasticsearch"
 )
-
-func RegisterPodBlockerWebhook(mgr ctrl.Manager) {
-	wh := &podBlockerWebhook{
-		client: mgr.GetClient(),
-	}
-	podLog.Info("Registering pod blocker webhook", "path", podBlockerWebhookPath)
-	mgr.GetWebhookServer().Register(podBlockerWebhookPath, &webhook.Admission{Handler: wh})
-}
 
 func RegisterPVCBlockerWebhook(mgr ctrl.Manager) {
 	wh := &pvcBlockerWebhook{
@@ -70,59 +57,15 @@ func (wh *pvcBlockerWebhook) Handle(_ context.Context, req admission.Request) ad
 		pvcLog.Error(err, fmt.Sprintf("error DecodeRaw/PVC for req %s/%s", req.Namespace, req.Name))
 		return admission.Errored(http.StatusBadRequest, err)
 	}
-	pvcLog.Info(fmt.Sprintf("Handling change for PVC %s/%s", pvc.Namespace, pvc.Name))
-	if pvc.Annotations == nil {
-		pvcLog.Info(fmt.Sprintf("Handling change for PVC %s/%s - no annotations: allowed", pvc.Namespace, pvc.Name))
-		return admission.Allowed("")
-	}
-	if req.Operation == admissionv1.Create {
-		pvcLog.Info(fmt.Sprintf("Handling PVC %s/%s CREATION [%+v]", pvc.Namespace, pvc.Name, pvc))
+	// TODO: should be fine to do _, delay := pvc.Annotations[DelayAnnotation] here
+	if req.Operation == admissionv1.Create && len(pvc.Annotations) > 0 {
 		_, delay := pvc.Annotations[DelayAnnotation]
 		if delay {
-			pvcLog.Info(fmt.Sprintf("PVC %s/%s CREATION is DELAYED", pvc.Namespace, pvc.Name))
-			// Also check if there is the trust annotation
-			_, trustedCreation := pvc.Annotations[TrustMeAnnotation]
-			if trustedCreation {
-				pvcLog.Info(fmt.Sprintf("PVC %s/%s CREATION is ALLOWED", pvc.Namespace, pvc.Name))
-				return admission.Allowed("this is an engineer")
-			}
-			pvcLog.Info(fmt.Sprintf("PVC %s/%s CREATION is STILL DELAYED", pvc.Namespace, pvc.Name))
+			// TODO: use k/v in logs
+			pvcLog.Info(fmt.Sprintf("%s on PVC %s/%s - DENIED", req.Operation, pvc.Namespace, pvc.Name))
 			return admission.Denied(fmt.Sprintf("Creation of PVC %s/%s delayed", pvc.Namespace, pvc.Name))
 		}
-		pvcLog.Info(fmt.Sprintf("Handling PVC %s/%s - no annotation - CREATION is ALLOWED", pvc.Namespace, pvc.Name))
 	}
-	return admission.Allowed("")
-}
-
-type podBlockerWebhook struct {
-	client  k8s.Client
-	decoder *admission.Decoder
-}
-
-var _ admission.DecoderInjector = &podBlockerWebhook{}
-
-// InjectDecoder injects the decoder automatically.
-func (wh *podBlockerWebhook) InjectDecoder(d *admission.Decoder) error {
-	wh.decoder = d
-	return nil
-}
-
-// Handle blocks creation of the temporary Pod and PVC until the temporary PVC is created from the original one
-func (wh *podBlockerWebhook) Handle(_ context.Context, req admission.Request) admission.Response {
-	pod := &corev1.Pod{}
-	err := wh.decoder.DecodeRaw(req.Object, pod)
-	if err != nil {
-		pvcLog.Error(err, fmt.Sprintf("error DecodeRaw/POD for req %s/%s", req.Namespace, req.Name))
-		return admission.Errored(http.StatusBadRequest, err)
-	}
-
-	if req.Operation == admissionv1.Create {
-		// Check vacate annotation on Pod
-
-		// Only allow Pod creation if PVC has been created
-
-		//return admission.Denied(err.Error())
-
-	}
+	pvcLog.Info(fmt.Sprintf("%s on PVC %s/%s - ALLOWED", req.Operation, pvc.Namespace, pvc.Name))
 	return admission.Allowed("")
 }
